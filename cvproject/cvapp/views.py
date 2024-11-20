1a01b233f7185f89f4cbf7027f67c19604e6712a
dahunsi-dami <gbemigadare@gmail.com>
2024-11-20 19:25:38 +0100

    Add view (webpage) to handle signups.

--- cvproject/cvapp/views.py
+++ cvproject/cvapp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     """The home view (i.e., web page) for the app."""
     return render(request, 'main/home.html')
+
+def sign_up(request):
+    """The signup view & its operation logic."""
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(
+            request,
+            'registration/sign_up.html',
+            {"form": form}
+    )
