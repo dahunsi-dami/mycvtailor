5df486bf3b80ab755dd63300de5251ce19775538
dahunsi-dami <gbemigadare@gmail.com>
2025-01-05 15:24:06 +0100

    Update signup URL to use CustomSignUpView for improved registration handling.

--- cvproject/cvapp/urls.py
+++ cvproject/cvapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('sign-up', views.sign_up, name='sign_up'),
+    path('signup/', views.CustomSignUpView.as_view(), name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.CustomLoginView.as_view(template_name='registration/login.html'), name='login'),
 #    path('logout', auth_views.LogoutView.as_view(), name='logout'),
