edde7d9613231928102720f9c9bd62dbaea48147
dahunsi-dami <gbemigadare@gmail.com>
2025-01-05 15:23:53 +0100

    Add custom signup view with AJAX support for seamless user registration.

--- cvproject/cvapp/views.py
+++ cvproject/cvapp/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page, never_cache, cache_control
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.vary import vary_on_cookie
+from django.views.generic.edit import CreateView
 from .forms import RegisterForm
 
 # Create your views here.
@@ -42,6 +45,7 @@ def logout_view(request):
 
 @method_decorator(cache_control(private=True, max_age=300), name='dispatch')
 class CustomLoginView(LoginView):
+    """Custom login view that leverages AJAX to prevent page reload."""
     template_name = 'registration/login.html'
     modal_template = 'registration/login_modal.html'
 
@@ -71,6 +75,47 @@ class CustomLoginView(LoginView):
         """
         Overrides custom method of LoginView to-
         -handle failed logins.
+        """
+        if self.request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return JsonResponse({
+                'success': False,
+                'errors': form.errors
+            })
+        return super().form_invalid(form)
+    
+class CustomSignUpView(CreateView):
+    """Custom signup view that leverages AJAX to prevent page reload."""
+    form_class = UserCreationForm
+    temmplate_name = 'registration/signup.html'
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests & returns the form.
+        """
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            form = self.get_form()
+            return render(request, 'registration/signup_modal.html', {'form': form})
+        return super().get(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        """
+        Overrides custom method of CreateView to-
+        -handle successful signup.
+        """
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        if self.request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return JsonResponse({
+                'success': True,
+                'redirect_url': self.success_url
+            })
+        return response
+    
+    def form_invalid(self, form):
+        """
+        Overrides custom method of CreateView to-
+        -handle failed signups.
         """
         if self.request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             return JsonResponse({
